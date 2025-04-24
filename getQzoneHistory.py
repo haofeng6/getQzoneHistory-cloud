33aece254dafba9a2760f466dab5cba8eae4c8cd
haofeng6 <wangh8982@gmail.com>
2025-04-24 16:43:33 +0800

    注册 fetch 接口

--- getQzoneHistory.py
+++ getQzoneHistory.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, jsonify
-import os
+from flask import Flask
+from api.fetch import fetch_bp  # 引入刚才创建的接口模块
 
 app = Flask(__name__)
+app.register_blueprint(fetch_bp)  # 注册接口
 
-@app.route("/")
-def home():
-    return "服务已启动，准备接入QQ空间数据接口..."
+@app.route('/')
+def index():
+    return '服务已启动，准备接入QQ空间数据接口...'
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run()
